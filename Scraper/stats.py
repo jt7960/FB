4e60e126d25d72fd45bf1e297211d6a77f161e90
alexandj <joel.alexander@hp.com>
2020-03-30 10:37:32 -0600

    moved schedule scraping code into a function

--- Scraper/stats.py
+++ Scraper/stats.py
@@ -11,34 +11,29 @@ import pandas as pd
 
 nba_months = ['october', 'november', 'december', 'january', 'feburary', 'march', 'april', 'june']
 
-nba_seasons = []
-blank_months = []
 
-for i in range(2000, 2020):
-    nba_seasons.append(str(i))
 
-for season in nba_seasons:
-    for month in nba_months:
-        #get_shedule('https://www.basketball-reference.com/leagues/NBA_',season,'_games-',month,'.html')
-        url = ('https://www.basketball-reference.com/leagues/NBA_'+season+'_games-'+month+'.html')
-        print(url)
-        req = requests.get(url)
-        html = req.text
-        try:
-            tables = pd.read_html(html)
-            print(tables)
-        except:
-            blank_months.append(url)
-            print('no tables found at ' + url)
-for month in blank_months:
-    print(month+'\n')
+# Scrape https://www.basketball-reference.com/leagues/ for NBA schedules from 2000 to 2019
+# Insert that data into the data base (use a seperate 'insert' function that can be re-used elsewhere)
 
-### proof that this works...
-#url = 'https://www.basketball-reference.com/leagues/NBA_2018_games-october.html'
-#req = requests.get(url)
-#html = req.text
-#tables = pd.read_html(html)
-#print(tables)
-### end proof
+def get_nba_schedule_history():
+    nba_seasons = []
+    blank_months = []
+    for i in range(2000, 2020):
+        nba_seasons.append(str(i))
 
-#https://www.basketball-reference.com/leagues/NBA_2018_games-october.html
+    for season in nba_seasons:
+        for month in nba_months:
+            #get_shedule('https://www.basketball-reference.com/leagues/NBA_',season,'_games-',month,'.html')
+            url = ('https://www.basketball-reference.com/leagues/NBA_'+season+'_games-'+month+'.html')
+            print(url)
+            req = requests.get(url)
+            html = req.text
+            try:
+                tables = pd.read_html(html)
+                print(tables)
+            except:
+                blank_months.append(url)
+                print('no tables found at ' + url)
+    for month in blank_months:
+        print(month+'\n')
